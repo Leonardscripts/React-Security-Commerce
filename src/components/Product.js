f4cb9d0cccacd0ba046ea1a4c35ff7c31520b313
mike <mikey12349876@gmail.com>
2019-05-09 10:40:53 -0400

    commit

--- src/components/Product.js
+++ src/components/Product.js
@@ -1,11 +1,17 @@
 import React, { Component } from "react";
+import styled from "styled-components";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 export default class Product extends Component {
   render() {
+    const { id, title, img, price, inCart } = this.props.product;
     return (
-      <div>
+      <ProductWrapper>
         <h3> hello from product</h3>
-      </div>
+      </ProductWrapper>
     );
   }
 }
+
+const ProductWrapper = styled.div``;
--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,9 @@
 import React, { Component } from "react";
 import Product from "./Product";
 import Title from "./Title";
-import { storeProducts } from "../data";
 import { ProductConsumer } from "../context";
 export default class ProductList extends Component {
-  state = {
-    products: storeProducts
-  };
   render() {
-    console.log(this.state.products);
-
     return (
       <React.Fragment>
         <div className="py-5">
@@ -18,15 +12,16 @@ export default class ProductList extends Component {
 
             <div className="row">
               <ProductConsumer>
-                {hello => {
-                  return <h1>{hello}</h1>;
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
                 }}
               </ProductConsumer>
             </div>
           </div>
         </div>
       </React.Fragment>
-      //  <Product />
     );
   }
 }
