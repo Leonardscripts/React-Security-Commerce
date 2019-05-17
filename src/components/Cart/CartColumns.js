a575e69eceb8bae00eccac7edb6b5342ec14b505
mike <mikey12349876@gmail.com>
2019-05-17 09:50:21 -0400

    commit

--- src/components/Cart/CartColumns.js
+++ src/components/Cart/CartColumns.js
@@ -7,6 +7,21 @@ export default function CartColumns() {
         <div className="col-10 mx-auto col-lg-2">
           <p className="text-uppercase">products</p>
         </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">name of product</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">price</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">quanity</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">remove</p>
+        </div>
+        <div className="col-10 mx-auto col-lg-2">
+          <p className="text-uppercase">total</p>
+        </div>
       </div>
     </div>
   );
