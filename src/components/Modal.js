5a17316261e6f3d1a6d0bb8b9ee81220faaaf642
mike <mikey12349876@gmail.com>
2019-05-19 01:16:44 -0400

    commit

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -52,7 +52,7 @@ const ModalContainer = styled.div`
   left: 0;
   right: 0;
   bottom: 0;
-  background: rgba(0, 0, 0, 0.3);
+  background: rgba(0, 0, 0, 0.8);
   display: flex;
   align-items: center;
   justify-content: center;
